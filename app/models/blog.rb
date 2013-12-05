fbb02e86cc7aee84344671cfe7ad010ff0201a3a
presidentJFK <davidmichaelbe@gmail.com>
2013-12-04 23:03:01 -0800

    added default image

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,12 @@
 class Blog < ActiveRecord::Base
 	validates_presence_of :title, :body
 	has_attached_file :pic, {
-		:styles => {:thumb =>["150x150#", :jpg]},
+		:styles => {:thumb =>["112x112#", :jpg]},
 		:convert_options => {
 			:medium => "-quality 80 -interlace Plane",
 			:thumb => "-quality 80 -interlace Plane"
-		}, :processors => [:thumbnail, :compression]
+		}, :processors => [:thumbnail, :compression],
+			:default_url => "missing.png"
 	} 
 
 	def time
