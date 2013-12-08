a24b5c0f833e4353e625970a3adeb02dc86a478c
presidentJFK <davidmichaelbe@gmail.com>
2013-12-07 16:37:49 -0800

    fixing broken production images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,14 @@
 David::Application.configure do
+	
+	# config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true  
   config.assets.digest = true
   config.assets.version = '1.0'
   config.log_level = :info
